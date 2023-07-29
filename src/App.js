311953fd396878cebdbf66bebc91e79ac3fd1942
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-28 20:16:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-     Weather App
      <Weather/>
      <footer>This project was coded by Ronna Del Rosario and is <a href="https://github.com/RonLi5321/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a></footer>
     </div>
