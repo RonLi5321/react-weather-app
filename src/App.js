53285ecfce856b874dc8ec7d21d6f9495b7e3baf
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-28 19:26:54 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
      Weather App
      <footer>This project was coded by Ronna Del Rosario and is <a href="https://github.com/RonLi5321/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a></footer>
+    </div>
     </div>
   );
 }
