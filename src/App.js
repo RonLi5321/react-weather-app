71c39372e6cab3a9eed9cd2bbe5f670207ea3dab
RonLi5321 <115131348+RonLi5321@users.noreply.github.com>
2023-07-29 23:43:21 -0700

    Revised defaultCity

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   
     <div className="App">
       <div className="container">
-     <Weather defaultCity="San Francisco"/>
+     <Weather defaultCity="{San Francisco}"/>
      </div>
      <div>
      <footer>This project was coded by Ronna Del Rosario and is <a href="https://github.com/RonLi5321/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a>
